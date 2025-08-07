f11d7839e3d6fd30f35dcd394c2a576ff1877017
Sarafinasarizz <bilaunisarah641@gmail.com>
2025-08-07 13:42:24 +0300

    changes

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -71,4 +71,50 @@ const temples = [
     "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
   },
   // Add more temple objects here...
+  {
+    templeName: "Accra Ghana",
+    location: " Accra, Ghana",
+    dedicated: "2004, January, 11",
+    area: 17500,
+    imageUrl: "images/accra_ghana_temple_lds.jpeg",
+  },
+
+  {
+    templeName: "Nairobi Kenya",
+    location: "Nairobi, Kenya",
+    dedicated: "2025, May, 18",
+    area: 17000,
+    imageUrl: "images/nairobi_kenya_temple_exterior.jpeg",
+  },
 ];
+
+createTempleCard();
+
+function createTempleCard() {
+  temples.forEach(temple => {
+    let card = document.createElement("section");
+    let name = document.createElement("h3");
+    let location = document.createElement("p");
+    let dedication = document.createElement("p");
+    let area = document.createElement("p");
+    let img = document.createElement("img")
+
+    name.textContent = temple.templeName;
+    location.innerHTML  =`<span class="label">Location:</span> ${temple.location}`;
+    dedication.innerHTML = `<span class="label">Dedicated:</span> ${temple.dedicated}`;
+    area.innerHTML = `<span class="label">Size:</span> ${temple.area} sq ft`;
+    img.setAttribute("src", temple.imageUrl);
+    img.setAttribute("alt", `${temple.templeName} Temple`);
+    img.setAttribute("loading", "lazy");
+
+    card.appendChild(name);
+    card.appendChild(location);
+    card.appendChild(dedication);
+    card.appendChild(area);
+    card.appendChild(img);
+
+    document.querySelector(".res-grid").appendChild(card);
+});
+}
+
+
